28ec8dbf7fddd2899fbe53029717900fe21b06f9
AntoBrandi <antonio.brandi@outlook.it>
2021-10-16 18:21:06 +0200

    updated simple publisher and subscriber and making it executables

--- Section9-Build_the_Real_Robot/arduinobot_ws/src/arduinobot_test/scripts/simple_publisher.py
+++ Section9-Build_the_Real_Robot/arduinobot_ws/src/arduinobot_test/scripts/simple_publisher.py
@@ -13,12 +13,14 @@ if __name__ == '__main__':
         # Define the frequency for publishing the messages
         # The rate is expressed in Hertz
         rate = rospy.Rate(10) # 10hz
+        count = 0
 
         # Keep going publishing messages until the ROS communication is alive
         while not rospy.is_shutdown():
-            hello_str = "hello world %s" % rospy.get_time()
+            hello_str = "hello world %d" % count
             rospy.loginfo(hello_str)
             pub.publish(hello_str)
+            count += 1
             # wait the desired rate before publishing the next message
             rate.sleep()
     except rospy.ROSInterruptException:
